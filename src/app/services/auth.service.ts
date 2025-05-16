216dcd7667129af3b04043078ff81ca9a529a380
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2025-05-16 17:07:27 +0200

    Creating User model and service for user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,1 +1,20 @@
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+
+  register(user: User): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, user);
+  }
+}
